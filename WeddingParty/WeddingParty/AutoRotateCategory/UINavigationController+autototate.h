4c883038234ea1cd4d591d151edd972a62b9196a
MindTheGap <cavnery@gmail.com>
2013-06-17 07:57:11 -0700

    fixed warnings and pictures and added images

---
+++ WeddingParty/WeddingParty/AutoRotateCategory/UINavigationController+autototate.h
@@ -0,0 +1,13 @@
+//
+//  UINavigationController+autototate.h
+//  WeddingParty
+//
+//  Created by MTG on 6/17/13.
+//  Copyright (c) 2013 MTG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UINavigationController (autototate)
+
+@end
---
+++ WeddingParty/WeddingParty/AutoRotateCategory/UINavigationController+autototate.m
@@ -0,0 +1,13 @@
+//
+//  UINavigationController+autototate.m
+//  WeddingParty
+//
+//  Created by MTG on 6/17/13.
+//  Copyright (c) 2013 MTG. All rights reserved.
+//
+
+#import "UINavigationController+autototate.h"
+
+@implementation UINavigationController (autototate)
+
+@end
